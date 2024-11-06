dff150e16b7ab49bb28368601a283f5f688c5988
Milan-1002 <sunarmilan1002@gmail.com>
2024-11-06 11:44:16 -0600

    All link list operation completed

--- linked_list_OP.c
+++ linked_list_OP.c
@@ -51,6 +51,133 @@ void insert_begin(){
     
     }
 }
+void insert_pos(){
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    printf("Enter the data for the new node\n");
+    scanf("%d",&newNode->data);
+    int pos;
+    printf("Enter the position you want to inser\n");
+    scanf("%d",&pos);
+    newNode->next = NULL;
+
+    // check if list is empty. if found empty insert node at the head
+    if(head == NULL){  
+        head = newNode;
+        return;
+    }
+    else{
+        struct node* current;
+        current = head;
+        for(int i = 0; i< pos-2; i++){
+            current = current->next;
+            if(current->next == NULL){
+                printf("The position is out of bound\n");
+                break;
+            }   
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+            
+
+    }
+}
+
+void insert_end(){
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    printf("Enter the data for the new node\n");
+    scanf("%d",&newNode->data);
+    newNode->next = NULL;
+    struct node* current;
+    current = head;
+    if(head == NULL){
+        head = newNode;
+    }
+    else{
+        
+        // Traverse to the end of the node
+        while(current->next != NULL){
+            current = current->next;
+        }
+        // point the next of current node to the new node
+        current->next = newNode;
+    
+    }
+}
+
+void delete_begin(){
+   
+    // check if the list is empty
+    if (head == NULL){
+        printf("The list is empty\n");
+        return;
+    }
+    else{
+        struct node* temp;  // temp variable to store the current head
+        temp = head;
+        head = head->next;
+        free(temp);     // free the previous head
+    }
+
+}
+
+void delete_end(){
+    // check if head is empty
+    if(head == NULL){
+        printf("the list is empty\n");
+        return;
+    }
+
+    // if there is only one node in the list
+    if(head->next == NULL){
+        free(head);
+        head = NULL;
+    }
+    else{
+        struct node* current = head;
+        while(current->next->next != NULL){
+            current = current->next;
+        }
+        // free the last node
+        free(current->next);
+        current->next = NULL;
+    }
+}
+
+void delete_pos(){
+    // check if the list is empty
+    if(head == NULL){
+        printf("The list is empty\n");
+        return;
+    }
+
+    int pos;
+    printf("Enter the position you want to delete\n");
+    scanf("%d",&pos);
+
+    // if list has only one node
+    if(head->next == NULL){
+        free(head);
+        head = NULL;
+        return;
+    }
+    else{
+        struct node* current = head;
+        struct node* temp;
+        for(int i = 0; i< pos-1; i++){
+            if(current->next == NULL){
+                printf("the position is out of bound\n");
+                return;
+            }
+            current = current->next;
+        }
+        
+        temp = current->next;
+        current->next = temp->next;
+        free(temp);
+
+    }
+}
+
 
 int main(){
 int choice;
@@ -80,22 +207,21 @@ case 1:
 case 2: 
     insert_begin();
     break;
-// case 3: 
-//     insert_pos();
-//     break;
-// case 4: 
-
-//     insert_end();
-//     break;
-// case 5: 
-//     delete_begin();
-//     break;
-// case 6: 
-//     delete_end();
-//     break;
-// case 7: 
-//     delete_pos();
-//     break;
+case 3: 
+    insert_pos();
+    break;
+case 4: 
+    insert_end();
+    break;
+case 5: 
+    delete_begin();
+    break;
+case 6: 
+    delete_end();
+    break;
+case 7: 
+    delete_pos();
+    break;
 case 8: 
     exit(0);
 default:
